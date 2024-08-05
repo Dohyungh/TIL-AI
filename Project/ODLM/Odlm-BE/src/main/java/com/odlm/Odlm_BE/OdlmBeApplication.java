4172877628be64c67281eeafcee0ea86d4bff7f2
Dohyungh <bley1217@gmail.com>
2024-08-06 08:41:51 +0900

    Init: BE Gradle 프로젝트 생성 -> Triton으로 이전 계획

---
+++ Project/ODLM/Odlm-BE/src/main/java/com/odlm/Odlm_BE/OdlmBeApplication.java
@@ -0,0 +1,13 @@
+package com.odlm.Odlm_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdlmBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdlmBeApplication.class, args);
+	}
+
+}
---
+++ Project/ODLM/Odlm-BE/src/test/java/com/odlm/Odlm_BE/OdlmBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odlm.Odlm_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdlmBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
